9feeaedea949f34ca9a2345782972688b8ac8275
Fery Wardiyanto <ferywardiyanto@gmail.com>
2024-10-23 01:37:47 +0700

    chore: define global test settings
    Signed-off-by: Fery Wardiyanto <ferywardiyanto@gmail.com>

--- tests/config.php
+++ tests/config.php
@@ -12,9 +12,23 @@ $cli->option('coverage', 'default', 3);
 $cli->option('spec', 'default', ['tests/spec']);
 $cli->option('lcov', 'default', 'tests/lcov.info');
 
+Filters::apply($this, 'run', function ($next) {
+    /** @var Kahlan\Cli\Kahlan $this */
+    $scope = $this->suite()->root()->scope(); // The top most describe scope.
+
+    $scope->settings = [
+        'local' => [
+            'path' => null,
+        ],
+    ];
+
+    return $next();
+});
+
 Filters::apply($this, 'reporting', function ($next) {
+    /** @var Kahlan\Cli\Kahlan $this */
     if (! $reporter = $this->reporters()->get('coverage')) {
-        return;
+        return $next();
     }
 
     $lcov_file = getenv('LCOV_REPORT') ?: $this->reporters()->get('lcov');
